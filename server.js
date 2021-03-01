2d79dda8a42a239e6da511ad98d2b7929c1634e1
OrchaniousS <bmagramovs3@gmail.com>
2021-03-01 19:23:05 +0200

    better regex expression for detecting url

--- server.js
+++ server.js
@@ -38,13 +38,15 @@ app.post(
   (request, response) => {
     let inputUrl = request.body["url"];
 
-    let urlRegex = new RegExp(
-      /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
-    );
+    let urlRegex = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
     if (!inputUrl.match(urlRegex)) {
-      response.json({ error: 'invalid url' });
-      return;
+      return response.json({ error: 'invalid url' });
     }
 
     responseObject["original_url"] = inputUrl;
